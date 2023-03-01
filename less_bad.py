a018f9c91f84717875bc0f48471405fe44a12d0f
vijay pandurangan <vijayp@vijayp.ca>
2023-02-28 16:44:15 -0800

    don't use unnecessary quotes.

--- less_bad.py
+++ less_bad.py
@@ -6,6 +6,6 @@ import shlex
 if __name__ == '__main__':
     molecule = input('enter the name of the molecule >')
     print('The molecule name has the following number of characters')
-    cmd_str = './counter.sh "%s"' % shlex.quote(molecule)
+    cmd_str = './counter.sh %s' % shlex.quote(molecule)
     print(cmd_str)
     os.system(cmd_str)
